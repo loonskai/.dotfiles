09390689bf75ccac329a6248e9b48039af5411a0
Siarhei Lunski <slunski@box.com>
2024-01-11 10:46:39 +0100

    zsh configs

---
+++ nvim/.config/nvim/after/plugin/jsonnet.lua
@@ -0,0 +1,1 @@
+vim.g.jsonnet_fmt_options = '-n 4 --string-style d'
--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -5,3 +5,20 @@ vim.keymap.set('n', '<leader>fl', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
+
+-- alternative: https://github.com/nvim-telescope/telescope.nvim/issues/1048#issuecomment-1679797700
+local actions = require('telescope.actions')
+require('telescope').setup{
+  defaults = {
+    mappings = {
+      i = {
+        ["<C-w>"] = actions.send_selected_to_qflist,
+        ["<C-q>"] = actions.send_to_qflist,
+      },
+      n = {
+        ["<C-w>"] = actions.send_selected_to_qflist,
+        ["<C-q>"] = actions.send_to_qflist,
+      },
+    },
+  }
+}
